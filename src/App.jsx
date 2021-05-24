6187f9d0b920e8fc6d7152485d8bf679a7b14761
Tyler Parker <tparker42d2@gmail.com>
2021-05-24 13:04:19 -0600

    Successfully mapped blogData to the BlogPost component and added an avatar source data point. This is a good stopping point for now.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { Grid, GridList, Typography, Card, CardMedia, CssBaseline, AppBar, Toolb
 import BlogPost from './BlogPost'
 import Footer from './Footer'
 import useStyles from './styles'
+import BlogData from './blogData'
 
 function App(props) {
     const classes = useStyles()
+    const postComponent = BlogData.map(post => <BlogPost post={post} />)
     return(
         <div>
             <CssBaseline />
@@ -19,9 +21,11 @@ function App(props) {
                 <Card >
                     <CardMedia className={classes.cardMedia} image='https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80' component='img'/>
                 </Card>
-                <GridList cellHeight={300} cols={1}>
-                    <BlogPost />
-                </GridList>
+                
+                    {postComponent}
+                
+                   
+                
 
             <Footer />
         </div>
--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Grid} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -7,11 +8,12 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import blogData from './blogData'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    maxWidth: '36ch',
+    // maxWidth: '36ch',
     backgroundColor: theme.palette.background.paper,
   },
   inline: {
@@ -19,76 +21,36 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function BlogPost() {
+export default function BlogPost(props) {
   const classes = useStyles();
 
   return (
-    <List className={classes.root}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Summer BBQ"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Oui Oui"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Sandra Adams
-              </Typography>
-              {' — Do you have Paris recommendations? Have you ever…'}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-    </List>
+    <Grid container xs={12}>
+        <List className={classes.root}>
+        <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+            primary={props.post.title}
+            secondary={
+                <React.Fragment>
+                <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                >
+                    {props.post.author}
+                </Typography>
+                {`-${props.post.subTitle}`}
+                </React.Fragment>
+            }
+            />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        </List>
+    </Grid>
+
   );
 }
